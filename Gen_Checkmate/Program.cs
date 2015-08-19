c5900f3caeae8a2f360d357aba0083fd654547c2
JamesXu <fur007@gmail.com>
2015-08-19 17:26:44 +1200

    Initial setup

---
+++ Gen_Checkmate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gen_Checkmate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in the input filename:");
+
+            Console.ReadKey();
+        }
+    }
+}
